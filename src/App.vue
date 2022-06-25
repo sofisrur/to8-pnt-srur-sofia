79834f5d31967230338025336e7e886f5f53f431
sofisrur <sofisrur1@gmail.com>
2022-06-25 18:28:07 -0300

    tp8

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container-fluid mt-3">
+    <div class="jumbotron">
+      <h1>Bienvenidos a VueJS CLI </h1>
+      <hr>
+      <Navbar/>
+      <router-view></router-view>
+   
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+   }
+}
+</script>
+
+<style>
+h1{
+  color:blue
+}
+
+
+
+</style>
